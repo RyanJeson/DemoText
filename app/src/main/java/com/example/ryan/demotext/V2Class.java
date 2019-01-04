57003b35b41695a88973e5b70326a64ea9754d62
ryan <ryan_jeson@outlook.com>
2019-01-04 23:06:37 +0800

    mac创建第二个分支V2

---
+++ app/src/main/java/com/example/ryan/demotext/V2Class.java
@@ -0,0 +1,16 @@
+package com.example.ryan.demotext;
+
+import android.util.Log;
+
+public class V2Class {
+
+
+    private static final String TAG = "V2Class";
+
+    public V2Class() {
+
+
+        Log.i(TAG, "V2Class: 这是第二个分支");
+
+    }
+}
