b33a660b47025a564b4311c9eaf6742316ab58b0
Ryan <ryan_jeson@outlook.com>
2019-01-04 16:37:22 +0800

    创建第一个分支V

---
+++ app/src/main/java/com/example/ryan/demotext/VClass.java
@@ -0,0 +1,19 @@
+package com.example.ryan.demotext;
+
+import android.util.Log;
+
+/**
+ * 文件描述：
+ * 创 建 人：Ryan
+ * 创建日期：2019-01-04
+ * 邮    箱：1258889302@qq.com
+ * 修改时间：
+ * 修改备注：
+ */
+public class VClass {
+    private static final String TAG = "VClass";
+
+    public VClass() {
+        Log.i(TAG, "VClass: 这是一个新的分支 ");
+    }
+}
